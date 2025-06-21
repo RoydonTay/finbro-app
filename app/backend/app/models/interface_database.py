7abee7afb9bc34c38fbdd80973ca591bea79ddda
yjiahao <yjiahao123@gmail.com>
2025-06-21 16:17:18 +0800

    update DDL

--- app/backend/app/models/interface_database.py
+++ app/backend/app/models/interface_database.py
@@ -1,25 +1,29 @@
 from psycopg2 import connect
 
+from sqlalchemy import create_engine, text
+
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-connection = connect(
-    dbname=os.getenv('POSTGRES_DB'),
-    user=os.getenv('POSTGRES_USER'),
-    password=os.getenv('POSTGRES_PASSWORD'),
-    host='localhost',
-    port=5432
+engine = create_engine(
+    # connection string
+    f"postgresql://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@localhost:5432/{os.getenv("POSTGRES_DB")}"
 )
 
-cursor = connection.cursor()
+with engine.connect() as conn:
+    result = conn.execute(
+        text(
+            '''
+            SELECT * FROM users u, holdings h
+            WHERE u.id = h.user_id; 
+            '''
+        )
+    )
 
-sql = '''
-SELECT * FROM users;
-'''
-
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-print(type(results[0]))
+rows = result.fetchall()
+for row in rows:
+    # we can just get the row attributes accordingly
+    print(row.user_id)
+    print(type(row))
